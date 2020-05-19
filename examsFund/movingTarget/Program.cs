44fcc19304ba48d66802058d948a2e379d93019c
Mehmet Ismail <60579835+drinoff@users.noreply.github.com>
2020-05-20 00:13:01 +0200

    Add files via upload

---
+++ examsFund/movingTarget/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace movingTarget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> target = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+            string[] command = input.Split();
+                int index = int.Parse(command[1]);
+                int actionPower = int.Parse(command[2]);
+                switch (command[0])
+                {
+                    case "Shoot":
+                        if (index < target.Count && target[index] != 0)
+                        {
+                            target[index] -= actionPower;
+                            if (target[index] <=0)
+                            {
+                                target.RemoveAt(index);
+                            }
+                        }
+                        else
+                        {
+
+                        }
+
+                        break;
+                    case "Add":
+                        if (index < target.Count)
+                        {
+                            target.Insert(index, actionPower);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid placement!");
+                        }
+
+                        break;
+                    case "Strike":
+                        if (index+actionPower< target.Count && index-actionPower>=0)
+                        {
+                            target.RemoveRange(index - actionPower, actionPower * 2 + 1);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Strike missed!");
+                        }
+
+                        break;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join("|", target));
+        }
+    }
+}
---
+++ examsFund/shootForWin/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace shootForWin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> target = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string command = Console.ReadLine();
+            int shotTargets = 0;
+
+            while (command != "End")
+            {
+                int index = int.Parse(command);
+
+                if (index < target.Count)
+                {
+                    if (target[index] != -1)
+                    {
+                        int temp = target[index];
+                        target[index] = -1;
+                        shotTargets++;
+                        for (int i = 0; i < target.Count; i++)
+                        {
+                            if (target[i] > temp)
+                            {
+                                if (target[i] != -1)
+                                {
+                                    target[i] -= temp;
+                                }
+                            }
+                            else
+                            {
+                                if (target[i] != -1)
+                                {
+                                    target[i] += temp;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else
+                {
+
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Shot targets: {shotTargets} -> {string.Join(" ", target)}");
+        }
+    }
+}
