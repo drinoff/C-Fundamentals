973de3c0e41f83f7811febce6eaf8dfc3cc112cb
Mehmet Ismail <drinoff@gmail.com>
2020-05-23 23:22:20 +0200

    update

--- arrays/maxSequenceOfEqualElements/Program.cs
+++ arrays/maxSequenceOfEqualElements/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks.Dataflow;
 
 namespace maxSequenceOfEqualElements
 {
@@ -8,8 +10,38 @@ namespace maxSequenceOfEqualElements
         static void Main(string[] args)
         {
             int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            
+            
 
-           
+            
+            int count = 1;
+            
+            int longestNum = input[0];
+            
+            int longestCount = 1;
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                
+                if (input[i] != input[i - 1])
+                {
+                    count = 0;
+                }
+
+                count++;
+                
+                if (count > longestCount)
+                {
+                    longestCount = count;
+                    longestNum = input[i];
+                }
+            }
+
+            
+            Console.WriteLine(
+                string.Join(" ", Enumerable.Repeat(longestNum, longestCount)));
+
+            
         }
     }
 }
