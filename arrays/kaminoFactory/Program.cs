b0198ed5af3be2560aa85297888b71c478dcc3ab
Mehmet Ismail <drinoff@gmail.com>
2020-06-02 22:18:12 +0200

    update

--- arrays/kaminoFactory/Program.cs
+++ arrays/kaminoFactory/Program.cs
@@ -12,13 +12,18 @@ namespace kaminoFactory
         {
             var dnaLength = Console.ReadLine();
             var input = Console.ReadLine();
+            var maxSequence = 0;
+            var bestSequence = new int[dnaLength.Length];
+            var bestLeftIndex = 0;
 
-
-            var bestcounter = 1;
-            var currentCounter = 1;
+            var bestCounter = 0;
+            var currentCounter = 0;
             int leftIndex = 0;
             var bestDna = new int[dnaLength.Length];
-
+            var result1 = 0;
+            var result2 = 0;
+            var anotherFokinCOunter = 0;
+            var bestSeqinputCount = 0;
 
             while (input != "Clone them!")
             {
@@ -27,28 +32,109 @@ namespace kaminoFactory
                     .Select(int.Parse)
                     .ToArray();
 
-                for (int i = 0; i < sequence.Length; i++)
+                for (int i = 0; i < sequence.Length-1; i++)
                 {
-                    if (i + 1 == i && i == 1)
+                    if (sequence[i] == 1)
+                    {
+                        if (sequence[i + 1] == 1)
+                        {
+                            currentCounter++;
+                        }
+                        else
+                        {
+                            currentCounter++;
+                            bestDna = sequence;
+                            bestCounter = currentCounter;
+                            leftIndex = i - currentCounter + 1;
+                            if (anotherFokinCOunter > 1)
+                            {
+                                bestLeftIndex = leftIndex;
+                            }
+                            
+                            
+                        }
+                    }
+                    else
+                    {
+                        currentCounter = 0;
+                    }
+                }
+                if (bestCounter > maxSequence)
+                {
+                    bestLeftIndex = leftIndex;
+                    bestSequence = bestDna;
+                    maxSequence = bestCounter;
+                    bestSeqinputCount++;
+                }
+                else if (bestCounter < maxSequence)
+                {
+
+                }
+                else
+                {
+                    if (leftIndex < bestLeftIndex)
+                    {
+                        bestLeftIndex = leftIndex;
+                        bestSequence = bestDna;
+                        maxSequence = bestCounter;
+                        bestSeqinputCount++;
+                    }
+                    else if (leftIndex == bestLeftIndex)
+                    {
+                        result1 = bestSequence.Sum();
+                        result2 = bestDna.Sum();
+                        if (result1 > result2)
+                        {
+
+                        }
+                        else if (result1 < result2)
+                        {
+                            bestLeftIndex = leftIndex;
+                            bestSequence = bestDna;
+                            maxSequence = bestCounter;
+                            bestSeqinputCount++;
+                        }
+                    }
+                }
+                bestCounter = 0;
+                currentCounter = 0;
+                leftIndex = 0;
+                anotherFokinCOunter++;
+
+                input = Console.ReadLine();
+            }
+            result1 = bestSequence.Sum();
+            Console.WriteLine($"Best DNA sample {bestSeqinputCount} with sum: {result1}.");
+            Console.WriteLine(string.Join(" ", bestSequence));
+
+        }
+        static int LongestSeqAndLeftIndex(int [])
+        {
+            for (int i = 0; i < sequence.Length - 1; i++)
+            {
+                if (sequence[i] == 1)
+                {
+                    if (sequence[i + 1] == 1)
                     {
                         currentCounter++;
                     }
                     else
                     {
-                        if (currentCounter > bestcounter)
+                        currentCounter++;
+                        bestDna = sequence;
+                        bestCounter = currentCounter;
+                        leftIndex = i - currentCounter + 1;
+                        if (anotherFokinCOunter > 1)
                         {
-                            bestDna = sequence;
-                            bestcounter = currentCounter;
-                            leftIndex = currentCounter - i;
-                            currentCounter = 1;
+                            bestLeftIndex = leftIndex;
                         }
-
-
-
                     }
                 }
-            }   
-
+                else
+                {
+                    currentCounter = 0;
+                }
+            }
         }
     }
 }
