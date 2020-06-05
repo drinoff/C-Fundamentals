8d3176b7bf89e16620320629ee9d638f4dbec85e
Mehmet Ismail <drinoff@gmail.com>
2020-06-05 19:04:30 +0200

    update

--- arrays/longestIncreasingSubsequence/Program.cs
+++ arrays/longestIncreasingSubsequence/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace longestIncreasingSubsequence
 {
@@ -6,7 +8,22 @@ namespace longestIncreasingSubsequence
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            var maxSeq = new Dictionary<int, List<int>>();
+            var firstKVP = new List<int>();
+            firstKVP.Add(numbers[0]);
+
+            maxSeq.Add(0, firstKVP);
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] > numbers[i - 1])
+                {
+                    
+                    
+                }
+            }
         }
     }
 }
---
+++ lists/cardGame/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace cardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstDeck = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var secondDeck = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            var deckQueue = new Queue<int>(firstDeck);
+            var deckQueue2 = new Queue<int>(secondDeck);
+            
+            while (true)
+            {
+                if (deckQueue.Count == 0)
+                {
+                    break;
+                }
+                if (deckQueue2.Count == 0)
+                {
+                    break;
+                }
+                var cardFirstDeck = deckQueue.Dequeue();
+                var cardSecondDeck = deckQueue2.Dequeue();
+
+                if (cardFirstDeck > cardSecondDeck)
+                {
+                    deckQueue.Enqueue(cardFirstDeck);
+                    deckQueue.Enqueue(cardSecondDeck);
+                }
+                else if (cardSecondDeck > cardFirstDeck)
+                {
+                    deckQueue2.Enqueue(cardSecondDeck);
+                    deckQueue2.Enqueue(cardFirstDeck);
+                }
+            }
+            if (deckQueue.Count == 0)
+            {
+                Console.WriteLine($"Second player wins! Sum: {deckQueue2.Sum()}");
+            }
+            else
+            {
+                Console.WriteLine($"First player wins! Sum: {deckQueue.Sum()}");
+            }
+        }
+    }
+}
