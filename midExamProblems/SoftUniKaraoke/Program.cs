055e25c51a44a235808f793e5ac45a0ea9079cc3
Mehmet Ismail <drinoff@gmail.com>
2020-07-02 20:16:48 +0200

    up

--- midExamProblems/SoftUniKaraoke/Program.cs
+++ midExamProblems/SoftUniKaraoke/Program.cs
@@ -14,14 +14,50 @@ namespace SoftUniKaraoke
             var input = Console.ReadLine();
             var awards = new Dictionary<string, List<string>>();
 
-            while (input!="dawn")
+            while (input != "dawn")
             {
                 var performance = input.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
                 var performer = performance[0];
                 var song = performance[1];
                 var award = performance[2];
 
+                if (participants.Contains(performer))
+                {
+                    if (songs.Contains(song))
+                    {
+                        if (awards.ContainsKey(performer))
+                        {
+                            if (!awards[performer].Contains(award))
+                            {
+                                awards[performer].Add(award);
+                            }
+                        }
+                        else
+                        {
+                            awards.Add(performer, new List<string> { award });
+                        }
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            if (awards.Values.Count == 0)
+            {
+                Console.WriteLine("No awards");
+            }
+            else
+            {
+                foreach (var item in awards.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+                {
+                    Console.WriteLine($"{item.Key}: {item.Value.Count} awards");
 
+                    var awardList = item.Value;
+
+                    foreach (var award in awardList.OrderBy(x => x))
+                    {
+                        Console.WriteLine($"--{award}");
+
+                    }
+                }
             }
         }
     }
