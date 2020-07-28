c9301db62b36025020cfeb5ad0384fa32135e054
Drinoff <drinoff@gmail.com>
2020-07-28 16:30:51 +0200

    update

---
+++ finalExams/songEnqryption/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace songEnqryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var pattern = @"^(?<name>[A-Z] ?[a-z]+ ?[a-z]+'? ?[a-z]+'? ?[a-z]+ ?)$";
+            var songPattern = @"^(?<song>[A-Z ]+)$";
+            StringBuilder sb = new StringBuilder();
+            while (input != "end")
+            {
+                var info = input.Split(":");
+                var artist = info[0];
+                var song = info[1];
+                var validMatch = Regex.Match(artist, pattern);
+                var validArtist = validMatch.Groups["name"].Value.ToCharArray();
+                var validSongMatch = Regex.Match(song, songPattern);
+                var validSong = validSongMatch.Groups["song"].Value.ToCharArray();
+                if (validArtist.Length != 0 && validSong.Length != 0)
+                {
+
+                    var key = validArtist.Length;
+                    var firstLetter = validArtist[0];
+                    if (firstLetter + key > 90)
+                    {
+                        var enqryptedFirstLetter = key - (90 - firstLetter) + 64;
+                        sb.Append((char)enqryptedFirstLetter);
+                    }
+                    else
+                    {
+                        sb.Append((char)(firstLetter + key));
+                    }
+                    for (int i = 1; i < validArtist.Length; i++)
+                    {
+                        if (validArtist[i] != ' ' && validArtist[i] != '\'')
+                        {
+                            var encrypted = validArtist[i] + key;
+                            if (encrypted > 122)
+                            {
+                                encrypted = key - (122 - validArtist[i]) + 96;
+                            }
+                            sb.Append((char)encrypted);
+                        }
+                        else
+                        {
+                            if (validArtist[i] == ' ')
+                            {
+                                sb.Append(" ");
+                            }
+                            else
+                            {
+                                sb.Append("'");
+                            }
+                        }
+                    }
+                    sb.Append("@");
+
+                    for (int i = 0; i < validSong.Length; i++)
+                    {
+                        if (validSong[i] != ' ' && validSong[i] != '\'')
+                        {
+                            var encrypted = validSong[i] + key;
+                            if (encrypted > 90)
+                            {
+                                encrypted = key - (90 - validSong[i]) + 64;
+                            }
+                            sb.Append((char)encrypted);
+                        }
+                        else
+                        {
+                            if (validSong[i] == ' ')
+                            {
+                                sb.Append(" ");
+                            }
+                            else
+                            {
+                                sb.Append("'");
+                            }
+                        }
+
+                    }
+                }
+                if (validMatch.Length == 0 || validSongMatch.Length == 0)
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+                else
+                {
+                    Console.WriteLine($"Successful encryption: {sb.ToString()}");
+                }
+                input = Console.ReadLine();
+                sb.Clear();
+            }
+        }
+    }
+}
---
+++ finalExams/warriorsQuest/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace warriorsQuest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var skill = Console.ReadLine();
+            var input = Console.ReadLine();
+            while (input != "For Azeroth")
+            {
+                var command = input.Split();
+                var action = command[0];
+                switch (action)
+                {
+                    case "GladiatorStance":
+                        skill = skill.ToUpper();
+                        Console.WriteLine(skill);
+                        break;
+                    case "DefensiveStance":
+                        skill = skill.ToLower();
+                        Console.WriteLine(skill);
+                        break;
+                    case "Dispel":
+                        var index = int.Parse(command[1]);
+                        char letter = char.Parse(command[2]);
+
+
+                        if (index >= 0 && index < skill.Length)
+                        {
+                            var skill2 = skill.ToCharArray();
+                            skill2[index] = letter;
+                            skill = string.Join("", skill2);
+                            Console.WriteLine("Success!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dispel too weak.");
+                        }
+
+                        break;
+                    case "Target":
+                        var subaction = command[1];
+                        switch (subaction)
+                        {
+                            case "Change":
+                                var subString = command[2];
+                                var secondSubString = command[3];
+                                skill = Regex.Replace(skill, subString, secondSubString);
+                                Console.WriteLine(skill);
+                                break;
+                            case "Remove":
+                                subString = command[2];
+                                skill = Regex.Replace(skill, subString, "");
+                                Console.WriteLine(skill);
+                                break;
+                            default:
+                                Console.WriteLine("Command doesn't exist!");
+                                break;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Command doesn't exist!");
+                        break;
+
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
